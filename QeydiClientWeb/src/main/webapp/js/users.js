09dcf87775312579c03f4273e8bc3d2129f4ef67
KhadijaGadirova <khadijagadirova1@gmail.com>
2023-04-06 16:42:40 +0400

    use strict js

--- QeydiClientWeb/src/main/webapp/js/users.js
+++ QeydiClientWeb/src/main/webapp/js/users.js
@@ -1,7 +1,4 @@
 "use strict";
-function goCv(email) {
-    window.location = 'cv?email=' + email;
-}
 
 function getToken() {
     let token = null;
@@ -20,23 +17,24 @@ function getToken() {
 }
 
 function getUsers() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let response = JSON.parse(this.responseText);
-            let list = response.obj;
-            printUsers(list);
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                let response = JSON.parse(this.responseText);
+                let list = response.name;
+                console.log(list);
+            }
         }
     };
     // var ema = document.getElementById("emailId").value;
     // var pass = document.getElementById("passwordId").value;
     // var user = {email: ema, password: pass};
-    xhttp.open("GET", "http://localhost:8089/QeydiRestApi_war_exploded/users", true);
+    xhr.open("GET", "http://localhost:8089/QeydiRestApi_war_exploded/users", true);
     xhr.setRequestHeader('Authorization', getToken());
 
-    xhttp.send();
-    // xhttp.send(JSON.stringify(user));
-    // window.location = 'users.html';
+    xhr.send();
+
 }
 function displayData(data) {
     const tableBody = document.getElementById('table-body');
