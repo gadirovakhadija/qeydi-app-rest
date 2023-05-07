fbbb67081f60ff9e0c34aa1d56036ad308e4c62b
KhadijaGadirova <khadijagadirova1@gmail.com>
2023-05-08 01:10:27 +0400

    some test changes

--- QeydiDbAppJpaSpring/src/test/java/org/example/UserServiceTest.java
+++ QeydiDbAppJpaSpring/src/test/java/org/example/UserServiceTest.java
@@ -1,48 +1,58 @@
 package org.example;
 
 import org.example.entity.User;
-import org.example.repo.UserRepositoryCustomImpl;
+import org.example.repo.UserRepositoryCustom;
 import org.example.service.impl.UserServiceImpl;
-import org.example.service.inter.UserServiceInter;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.MockitoJUnitRunner;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
+//@RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 
     @Mock
-    private UserServiceInter userDao;
+    private UserRepositoryCustom userRepoCust;
 
     @InjectMocks
     private UserServiceImpl userService;
 
+    @BeforeClass
+    public static void setUp(){
+        System.out.println("setup called");
+    }
+
     @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Before
+    public void before() {
+        List<User> list = new ArrayList<>();
+        User u = new User();
+        u.setName("Khadija");
+        u.setSurname("GAdirova");
+        u.setEmail("khady@gmail.com");
+        list.add(u);
+        Mockito.when(userRepoCust.getAll(null,null,null)).thenReturn(list);
     }
 
     @Test
     public void testGivenNullThenGetAll() {
         System.out.println("Testin icerisi");
         List<User> list = userService.getAll(null, null,null);
-        assertEquals(51, list.size(),"user size must be 2");
-    }
-    @Test
-    public void testGetAll() {
-
-        List<User> userList = userService.getAll("John", "Doe", null);
-
-        Assert.assertEquals("User list must contain 1 user", 1, userList.size());
-        Assert.assertEquals("User name must be John", "John", userList.get(0).getName());
-        Assert.assertEquals("User surname must be Doe", "Doe", userList.get(0).getSurname());
+//        assertEquals(1, list.size(),"user size must be 1");
+        Assert.assertEquals("user size must be 1",1, list.size());
     }
 }
